75fc5baa710b05ffc918a700f283d2551188f7d9
BenardBett <kiplangatbett5@gmail.com>
2020-11-07 12:09:10 +0300

    deleted form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
-import { FormComponentComponent } from './form-component/form-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    FormComponentComponent
+
   ],
   imports: [
     BrowserModule,
