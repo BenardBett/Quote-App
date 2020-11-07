fe690788d6ac95ef1ef52f03fc7492fa5fdaeebb
BenardBett <kiplangatbett5@gmail.com>
2020-11-07 19:21:22 +0300

    created an output event emitter

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-quote-details',
@@ -7,6 +7,13 @@ import { Component, OnInit, Input} from '@angular/core';
 })
 export class QuoteDetailsComponent implements OnInit {
 @Input() quote = Quote;
+@Output()deleteQuote = new EventEmitter<boolean>();
+
+
+ delete(removeQuote = boolean ){
+  this.deleteQuote.emit(removeQuote);
+}
+
   constructor() { }
 
   ngOnInit(): void {
